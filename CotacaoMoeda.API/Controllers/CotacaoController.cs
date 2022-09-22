62eba62c2e0395bfd103d1970972b21d66056893
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-09-21 22:40:26 -0300

    Add project files.

---
+++ CotacaoMoeda.API/Controllers/CotacaoController.cs
@@ -0,0 +1,43 @@
+﻿using CotacaoMoeda.Modelo;
+using CotacaoMoeda.Negocio.CotacaoNegocio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CotacaoMoeda.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CotacaoController : ControllerBase
+    {
+
+        #region Construtores
+        private readonly ICotacaoNegocio _CotacaoNegocio;
+
+        public CotacaoController(ICotacaoNegocio context)
+        {
+            _CotacaoNegocio = context;
+        }
+        #endregion
+
+        [HttpPost]
+        public async Task Incluir([FromBody] Cotacao cotacao)
+        {
+            cotacao.DataHoraCotacao = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                await _CotacaoNegocio.Incluir(cotacao);
+            }
+        }
+
+        //[HttpGet]
+        //public async Task<List<Cotacao>> Get()
+        //{
+        //    return await _CotacaoNegocio.ObterLista();
+        //}
+
+        [HttpGet]
+        public async Task<List<Cotacao>> Get() => await _CotacaoNegocio.ObterLista();
+
+
+    }
+}
---
+++ CotacaoMoeda.Negocio/CotacaoNegocio/CotacaoNegocio.cs
@@ -0,0 +1,31 @@
+﻿using CotacaoMoeda.Infra.Entity;
+using CotacaoMoeda.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace CotacaoMoeda.Negocio.CotacaoNegocio
+{
+    public class CotacaoNegocio : ICotacaoNegocio
+    {
+
+        #region Construtores
+        private readonly AppDbContext _context;
+
+        public CotacaoNegocio(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        #endregion
+
+        public async Task Incluir(Cotacao cotacao)
+        {
+            await _context.Cotacao.AddAsync(cotacao);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<Cotacao>> ObterLista()
+        {
+            return await _context.Cotacao.ToListAsync();
+        }
+
+
+    }
+}
